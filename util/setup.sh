c9181f2b59ae0226e186450066bf7cb565fd8be8
Colin Snover <github.com@zetafleet.com>
2011-03-03 14:55:25 -0600

    Update setup.sh to get RequireJS (and stop building release version of dojo, as the async loader works much better in dev). Fixes #1.

--- util/setup.sh
+++ util/setup.sh
@@ -4,9 +4,21 @@ set -e
 
 OUTDIR=../js
 VERSION="1.6.0"
+RJSVERSION="0.23.0"
+
 DOJODIR="dojo-release-${VERSION}-src"
+REQUIREJSDIR="requirejs-${RJSVERSION}"
 
 OUTDIR=$(cd "$OUTDIR" &> /dev/null && pwd || echo "")
+
+if [ -x $(which wget) ]; then
+	GET="wget --no-check-certificate -O -"
+elif [ -x $(which curl) ]; then
+	GET="curl -L --insecure -o -"
+else
+	echo "No cURL, no wget, no downloads :("
+	exit 1
+fi
 
 if [ "$OUTDIR" = "" ]; then
 	echo "Output directory not found"
@@ -14,25 +26,18 @@ if [ "$OUTDIR" = "" ]; then
 fi
 
 if [ ! -d "$OUTDIR/$DOJODIR" ]; then
-	echo "Retrieving dojo $VERSION"
-
-	if [ -x $(which wget) ]; then
-		wget -O - http://download.dojotoolkit.org/release-$VERSION/$DOJODIR.tar.gz | tar -C "$OUTDIR" -xzf -
-	elif [ -x $(which curl) ]; then
-		curl -o - http://download.dojotoolkit.org/release-$VERSION/$DOJODIR.tar.gz | tar -C "$OUTDIR" -xzf -
-	else
-		echo "No cURL, no wget, no downloads :("
-		exit 1
-	fi
-
+	echo "Retrieving Dojo $VERSION"
+	$GET http://download.dojotoolkit.org/release-$VERSION/$DOJODIR.tar.gz | tar -C "$OUTDIR" -xzf -
 	echo "Dojo extracted to $OUTDIR/$DOJODIR"
 fi
 
-if [ ! -d "$OUTDIR/dojo-release-$VERSION" ]; then
-	echo "Building minified dojo-release-$VERSION"
-	cd "$OUTDIR/$DOJODIR/util/buildscripts"
-	./build.sh profile=standard version=$VERSION action=release releaseName=dojo-release-$VERSION "releaseDir=$OUTDIR"
-	cd - &> /dev/null
+if [ ! -d "$OUTDIR/$REQUIREJSDIR" ]; then
+	echo "Retrieving RequireJS $RJSVERSION"
+	$GET http://requirejs.org/docs/release/$RJSVERSION/requirejs-$RJSVERSION.zip > \
+		"$OUTDIR/requirejs-$RJSVERSION.zip" && \
+	unzip "$OUTDIR/requirejs-$RJSVERSION.zip" -d "$OUTDIR" > /dev/null
+	rm "$OUTDIR/requirejs-$RJSVERSION.zip"
+	echo "RequireJS extracted to $OUTDIR/$REQUIREJSDIR"
 fi
 
 echo "Done!"
