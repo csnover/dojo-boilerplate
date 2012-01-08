82c0b661718e7ed2f727ad3a26e0e9fb32341d7e
Colin Snover <github.com@zetafleet.com>
2012-01-08 00:41:29 +0000

    Reduce number of HTTP requests by 1. (Yay!)

--- src/app/app.profile.js
+++ src/app/app.profile.js
@@ -1,13 +1,15 @@
 /**
- * This is a new Dojo 1.7 style build profile.
+ * This is a new Dojo 1.7 style build profile. Look at util/build/buildControlDefault.js if you want to explore the
+ * default Dojo build profile options.
  */
 
 // This function is used to determine whether or not a resource should be tagged as copy-only. See the resourceTags
 // property below for more information.
 function copyOnly(mid) {
-    // Ensures that our boot script is not transformed by the build system.
     return mid in {
-        'app/run': 1
+        // There are no modules right now in dojo boilerplate that are copy-only. If you have some, though, just add
+        // them here like this:
+        // 'app/module': 1
     };
 }
 
@@ -18,8 +20,8 @@ var profile = {
     basePath: '..',
 
     // This is the directory within the release directory where built packages will be placed. The release directory
-    // itself is defined by build.sh. By default, dbp doesn’t use this because root is where packages live, but
-    // if you want to move the packages somewhere else, this is how to do it. (You will also need to update build.sh.)
+    // itself is defined by build.sh. You really probably should not use this; it is a legacy option from very old
+    // versions of Dojo (like, version 0.1). If you do use it, you will need to update build.sh, too.
     // releaseName: '',
 
     // Builds a new release.
@@ -35,6 +37,9 @@ var profile = {
     // Note that you will probably get some “errors” with CC; these are generally safe to ignore, and will be
     // fixed in a later version of Dojo.
     optimize: 'closure',
+
+    // We’re building layers, so we need to set the minifier to use for those, too. This defaults to "shrinksafe" if
+    // it is not provided.
     layerOptimize: 'closure',
 
     // Strips all calls to console functions within the code. You can also set this to "warn" to strip everything
@@ -42,7 +47,9 @@ var profile = {
     stripConsole: 'all',
 
     // The default selector engine is not included by default in a dojo.js build in order to make mobile builds
-    // smaller. We add it back here to avoid that extra HTTP request.
+    // smaller. We add it back here to avoid that extra HTTP request. There is also a "lite" selector available; if
+    // you use that, you’ll need to set selectorEngine in app/run.js too. (The "lite" engine is only suitable if you
+    // are not supporting IE7 and earlier.)
     selectorEngine: 'acme',
 
     // Builds can be split into multiple different JavaScript files called “layers”. This allows applications to
@@ -50,7 +57,7 @@ var profile = {
     // be compiled into a single file.
     layers: {
         // This is the main application layer. This layer will normally contain most or all of your application code.
-        'app/main': { include: [ 'app/main' ] },
+        'app/run': { include: [ 'app/main', 'app/run' ] },
 
         // In the demo application, we conditionally require app/Dialog on the client-side, so we’re building a
         // separate layer containing just that client-side code.
--- src/app/run.js
+++ src/app/run.js
@@ -27,6 +27,12 @@ require({
 
         // For reference, this is what a more verbose package declaration looks like.
         { name: 'dbp', location: 'dbp', packageMap: {} }
-    ]
+    ],
+
+    // This is a hack. In order to allow app/main and app/run to be built together into a single file, a cache key needs
+    // to exist here in order to force the loader to actually process the other modules in the file. Without this hack,
+    // the loader will think that code for app/main has not been loaded yet and will try to fetch it again, resulting in
+    // a needless extra HTTP request.
+    cache: {}
 // Require 'app'. This loads the main application file, app/main.js.
 }, [ 'app' ]);
